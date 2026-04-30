d8a36550eb5496ca37a41cee43c23df5f79c6f81
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-04-30 15:52:52 +0300

    Test the whole return value in "foo" test

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -4,8 +4,8 @@ import { Item, Shop } from "../src/gilded_rose.mjs";
 
 describe("Gilded Rose", () => {
   test("foo", () => {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const gildedRose = new Shop([new Item("foo", 0, 3)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("foo");
+    expect(items).to.deep.equal([{ name: "foo", sellIn: -1, quality: 1 }]);
   });
 });
