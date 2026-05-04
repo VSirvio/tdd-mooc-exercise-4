46867e5a3eaf27f3bf9468ce301e88f74713c231
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-04 13:22:39 +0300

    Refactor #10

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -45,14 +45,14 @@ export class Shop {
             this.items[i].quality++;
           }
         } else {
-          if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+          if (this.items[i].name === "Backstage passes to a TAFKAL80ETC concert") {
+            this.items[i].quality = 0;
+          } else {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
                 this.items[i].quality--;
               }
             }
-          } else {
-            this.items[i].quality = 0;
           }
         }
       }
