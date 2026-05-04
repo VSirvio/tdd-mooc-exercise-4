bfc5b8d554337a25e6f43dc6862fc5c7f766e72d
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-04 13:36:32 +0300

    Refactor #26

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -38,7 +38,7 @@ export class Shop {
           this.items[i].quality--;
         }
       }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+      if (this.items[i].name != SULFURAS) {
         this.items[i].sellIn--;
       }
       if (this.items[i].sellIn < 0) {
