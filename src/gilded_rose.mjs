f5d5718b0630b4fef04b4ebc79ffdbde85f21894
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-04 14:11:49 +0300

    Refactor #71

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -47,7 +47,7 @@ export class Shop {
         if (item.sellIn < 0) {
           item.quality = 0;
         }
-      } else if (item.name !== SULFURAS) {
+      } else if (![AGED_BRIE, BACKSTAGE_PASSES, SULFURAS].includes(item.name)) {
         if (item.quality > 0) {
           item.quality--;
         }
