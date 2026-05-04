812a228e682822715e31f7ae54bc616e8f50f47f
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-04 17:05:45 +0300

    Refactor #115

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -25,7 +25,7 @@ export class Shop {
         if (item.sellIn < 0) {
           if (item.quality < 49) {
             item.quality += 2;
-          } else if (item.quality < 50) {
+          } else if (item.quality === 49) {
             item.quality += 1;
           }
         } else if (item.sellIn >= 0 && item.quality < 50) {
