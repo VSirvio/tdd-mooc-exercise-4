6905f7a4fdf1a5d9f4d527df9d43d644dfc541e2
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-04 17:07:56 +0300

    Refactor #121

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -25,10 +25,6 @@ export class Shop {
         if (item.sellIn < 0) {
           if (item.quality < 48) {
             item.quality += 2;
-          } if (item.quality === 48) {
-            item.quality = 50;
-          } else if (item.quality === 49) {
-            item.quality = 50;
           }
 
           if (item.quality > 50) {
