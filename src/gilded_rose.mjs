59c7f9f0d1d82a03dc2103edff89bec568daa6c1
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-04 13:38:44 +0300

    Refactor #39

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -22,7 +22,7 @@ export class Shop {
       if (item.name === AGED_BRIE || item.name === BACKSTAGE_PASSES) {
         if (item.quality < 50) {
           item.quality++;
-          if (this.items[i].name === BACKSTAGE_PASSES) {
+          if (item.name === BACKSTAGE_PASSES) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality++;
